033a36f92c811218574c980285bc3b6f7f1ab3f2
ISTEK <200693214+istekdev@users.noreply.github.com>
2026-01-02 22:56:17 -0600

    Create aes.py

---
+++ security/aes.py
@@ -0,0 +1,38 @@
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from hashlib import pbkdf2_hmac
+from pathlib import Path
+import os, json
+
+with open("./config.json", "r") as r:
+  config = json.load(r)
+
+def encrypt(user, password):
+  with open(f"/data/users/{user}.dat", "rb") as r:
+    user = r.read()
+  salt = os.urandom(16)
+  nonce = os.urandom(12)
+  psw = bytearray(pbkdf2_hmac("sha256", password.encode("utf-8"), salt, dklen=32))
+
+  aes = AESGCM(psw)
+  ciphertext = salt + nonce + aes.encrypt(nonce, Path(f"/data/users/{user}.dat"), None)
+  with open(f"/data/users/{user}.dat", "wb") as w:
+    w.write(ciphertext)
+
+  psw[:] = b"\x00" * len(psw)
+  del psw
+
+def decrypt(user, password):
+  with open(f"/data/users/{user}.dat", "rb") as r:
+    user = r.read()
+  salt = user[0:16]
+  nonce = user[16:28]
+  blob = user[28:]
+  psw = bytearray(pbkdf2_hmac("sha256", password.encode("utf-8"), salt, dklen=32))
+
+  aes = AESGCM(psw)
+  plaintext = aes.decrypt(nonce, blob, None).decode("utf-8")
+  with open(f"/data/users/{user}.dat", "w") as w:
+    json.dump(plaintext, w, indent=4)
+
+  psw[:] = b"\x00" * len(psw)
+  del psw
